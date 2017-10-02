1ca3c6683650232d694c1928bf65aef70f3c01c6
peytonchance <peytonchance@gmail.com>
2017-10-02 18:11:36 -0400

    Removed Tabletop and added Vue CDN.

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function init() {
-  Tabletop.init( { key: '1cVczPbMPcsWx9pPTqYiibmtSC41nZa1uaz0IrrqD3Ok',
-                   callback: function(data, tabletop) {
-                       console.log(data)
-                       console.log(data[0]['This is an item']);
-                   },
-                   simpleSheet: true } )
-}
-window.addEventListener('DOMContentLoaded', init)
